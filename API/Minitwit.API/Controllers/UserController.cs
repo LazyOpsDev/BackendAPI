bab19b34f530ab9e938ca56c1d5eda7cec6de2d6
Philip <phko@itu.dk>
2020-02-18 14:20:42 +0100

    follow unfollow

--- API/Minitwit.API/Controllers/UserController.cs
+++ API/Minitwit.API/Controllers/UserController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Minitwit.API.Util;
 using System;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace Minitwit.API.Controllers
@@ -17,34 +18,76 @@ namespace Minitwit.API.Controllers
             _userRepository = userRepository;
         }
         
-        [HttpPost]
-        [Route("{id}/follow")]
-        public async Task<IActionResult> Follow(string id) {
-            //If user not logged in 
-            if (!CookieHandler.LoggedIn(Request) || !Guid.TryParse(Request.Cookies["userId"].ToString(), out var UserId))
-                return Unauthorized();
+        //[HttpPost]
+        //[Route("{id}/follow")]
+        //public async Task<IActionResult> Follow(string id, int i) {
+        //    //If user not logged in 
+        //    if (!CookieHandler.LoggedIn(Request) || !Guid.TryParse(Request.Cookies["userId"].ToString(), out var UserId))
+        //        return Unauthorized();
 
-            //If user not found
-            if (!await _userRepository.FollowUser(Guid.NewGuid(), id))
+        //    //If user not found
+        //    if (!await _userRepository.FollowUser(Guid.NewGuid(), id))
+        //        return NotFound();
+
+        //    //TODO return correct status code
+        //    return RedirectToAction("UserTimeline", "Timeline");
+        //}
+
+        //[HttpPost]
+        //[Route("{id}/unfollow")]
+        //public async Task<IActionResult> Unfollow(string id) {
+        //    //If user not logged in 
+        //    if (!CookieHandler.LoggedIn(Request) || !Guid.TryParse(Request.Cookies["userId"].ToString(), out var UserId))
+        //        return Unauthorized();
+
+        //    //If user not found
+        //    if (! await _userRepository.UnfollowUser(Guid.NewGuid(), id))
+        //        return NotFound();
+
+        //    //TODO return correct status code
+        //    return Ok();
+        //}
+
+        [HttpPost]
+        [Route("fllws/{username}")]
+        public async Task<IActionResult> Follow([FromBody]followModel follow, string username)
+        {
+            //TODO maybe auth
+            //If user not logged in 
+            //if (!CookieHandler.LoggedIn(Request) || !Guid.TryParse(Request.Cookies["userId"].ToString(), out var UserId))
+            //    return Unauthorized();
+
+            if (string.IsNullOrEmpty(follow.follow) && !await _userRepository.UnfollowUser(Guid.NewGuid(), username))
+                return NotFound();
+            else if(string.IsNullOrEmpty(follow.follow) && !await _userRepository.FollowUser(Guid.NewGuid(), username))
                 return NotFound();
 
-            //TODO return correct status code
-            return RedirectToAction("UserTimeline", "Timeline");
+            return NoContent();
         }
 
-        [HttpPost]
-        [Route("{id}/unfollow")]
-        public async Task<IActionResult> Unfollow(string id) {
-            //If user not logged in 
-            if (!CookieHandler.LoggedIn(Request) || !Guid.TryParse(Request.Cookies["userId"].ToString(), out var UserId))
-                return Unauthorized();
+        //[HttpPost]
+        //[Route("fllws/{username}")]
+        //public async Task<IActionResult> UnFollow([FromBody]unFollowModel unfollow, string username)
+        //{
+        //    //If user not logged in 
+        //    //if (!CookieHandler.LoggedIn(Request) || !Guid.TryParse(Request.Cookies["userId"].ToString(), out var UserId))
+        //    //    return Unauthorized();
 
-            //If user not found
-            if (! await _userRepository.UnfollowUser(Guid.NewGuid(), id))
-                return NotFound();
+        //    //If user not found
+        //    if (!await _userRepository.UnfollowUser(Guid.NewGuid(), username))
+        //        return NotFound();
 
-            //TODO return correct status code
-            return Ok();
-        }
+        //    //TODO return correct status code
+        //    return RedirectToAction("UserTimeline", "Timeline");
+        //}
+    }
+    public class followModel
+    {
+        public string follow { get; set; }
+        public string unfollow { get; set; }
+    }
+
+    public class unFollowModel
+    {
     }
 }
