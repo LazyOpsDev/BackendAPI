ea3e2e0d63355cf42141d4886038f11b89e1bfd3
Philip <phko@itu.dk>
2020-02-20 15:49:55 +0100

    removed comments in db context

--- API/Minitwit.DataAccessLayer/CustomDbContext.cs
+++ API/Minitwit.DataAccessLayer/CustomDbContext.cs
@@ -14,12 +14,7 @@ namespace Minitwit.DataAccessLayer
 
         protected override void OnConfiguring(DbContextOptionsBuilder options)
         {
-            // @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Mini;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False;"
-            var connection = @"Data Source=localhost,11433 ;Initial Catalog=MyDB;Uid=;Password=p@55w0rd;";
-            //options.UseMySQL("Server=localhost;Database=minitwit;Uid=root;Pwd=hej123");//UseSqlServer(connection);
             options.UseMySql("Server=localhost;Database=minitwit;Uid=root;Pwd=hej123");                            //
-            //Server=localhost;Database=minitwit;Uid=root;Pwd=hej123;
-
         }
     }
 }
