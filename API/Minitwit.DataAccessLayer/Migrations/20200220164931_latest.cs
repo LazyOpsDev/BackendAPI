1bfd179c82c611c92ba7f624baceecd9384af3ce
Philip <phko@itu.dk>
2020-02-20 18:21:20 +0100

    docker compose

---
+++ API/Minitwit.DataAccessLayer/Migrations/20200220164931_latest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Minitwit.DataAccessLayer.Migrations
+{
+    public partial class latest : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Latest",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    latest = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Latest", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Latest");
+        }
+    }
+}
