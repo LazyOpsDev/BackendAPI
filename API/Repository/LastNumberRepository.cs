e336dcb9cc6613a95efd99aaf22cba1a6b1b5d7f
Philip <phko@itu.dk>
2020-02-20 16:37:07 +0100

    lastnumberRepository implementation

--- API/Repository/LastNumberRepository.cs
+++ API/Repository/LastNumberRepository.cs
@@ -1,10 +1,38 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Infrastructure.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Minitwit.DataAccessLayer;
+
 namespace Repository
 {
-    public class LastNumberRepository
+    public class LastNumberRepository : ILastNumberRepository
     {
-        public LastNumberRepository()
+        public async Task<int> ReadLatest()
         {
+            return 0;
+            using (var ctx = new CustomDbContext())
+            {
+                var r = await ctx.Latest.FirstOrDefaultAsync();
+                return r?.latest ?? 0;
+            }
+        }
+
+        public async Task WriteLatest(int i)
+        {
+            return;
+            using (var ctx = new CustomDbContext())
+            {
+                var r = await ctx.Latest.FirstOrDefaultAsync();
+                if (r == null)
+                {
+                    ctx.Latest.Add(new Minitwit.Models.LatestModel { latest = i });
+                    await ctx.SaveChangesAsync();
+                    return;
+                }
+                r.latest = i;
+                ctx.Update(r);
+            }
         }
     }
 }
