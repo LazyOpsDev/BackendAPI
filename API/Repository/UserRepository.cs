25b262f1e53c802cc0c2dccde07a5a9b715942e2
Marek Kisiel <mr.kislun@gmail.com>
2020-02-20 16:30:55 +0100

    Added Connection string

--- API/Repository/UserRepository.cs
+++ API/Repository/UserRepository.cs
@@ -11,65 +11,59 @@ namespace Repository
 {
     public class UserRepository : IUserRepository
     {
+        private readonly CustomDbContext _context;
+
+        public UserRepository(CustomDbContext context)
+        {
+            _context = context;
+        }
         public async Task<bool> FollowUser(string username, string follows)
         {
-            //return true;
-            using (var ctx = new CustomDbContext())
-            {
-                var userWho = await ctx.Users.FirstOrDefaultAsync(u => u.Username == username);
-                if (userWho == null)
-                    throw new ArgumentException();
-                
-                var followsUser = await ctx.Users.FirstOrDefaultAsync(u => u.Username == follows);
-                if (followsUser == null)
-                    throw new ArgumentException();
+            var userWho = await _context.Users.FirstOrDefaultAsync(u => u.Username == username);
+            if (userWho == null)
+                throw new ArgumentException();
+            
+            var followsUser = await _context.Users.FirstOrDefaultAsync(u => u.Username == follows);
+            if (followsUser == null)
+                throw new ArgumentException();
 
-                var follower = new Follower { Self = userWho, Following = followsUser };
-                ctx.Followers.Add(follower);
+            var follower = new Follower { Self = userWho, Following = followsUser };
+            _context.Followers.Add(follower);
 
-                return await ctx.SaveChangesAsync () != 0;
-            }
+            return await _context.SaveChangesAsync () != 0;
         }
 
         public async Task<Guid> Login(LoginModel user)
         {
-            //return Guid.Empty;
-            using (var ctx = new CustomDbContext())
-            {
-                var users = await ctx.Users.FirstOrDefaultAsync(u => u.Username == user.Username);
-                if (users == null)
-                    throw new ArgumentException();
+            var users = await _context.Users.FirstOrDefaultAsync(u => u.Username == user.Username);
+            if (users == null)
+                throw new ArgumentException();
 
-                if (PasswordHandler.Validate(user.Password, users.PasswordHash))
-                    return users.UserId;
-            }
+            if (PasswordHandler.Validate(user.Password, users.PasswordHash))
+                return users.UserId;
             
             return Guid.NewGuid();
         }
 
         public async Task<Guid> RegisterUser(RegisterModel user)
         {
-            //return Guid.Empty;
-            using (var ctx = new CustomDbContext())
+            var users = _context.Users.Where(u => u.Username == user.username);
+            if (users.Any())
+                throw new ArgumentException();
+
+            var passwordHash = PasswordHandler.CreatePasswordHash(user.pwd);
+
+            var usr = new User()
             {
-                var users = ctx.Users.Where(u => u.Username == user.username);
-                if (users.Any())
-                    throw new ArgumentException();
+                Username = user.username,
+                Email = user.email,
+                PasswordHash = passwordHash
+            };
 
-                var passwordHash = PasswordHandler.CreatePasswordHash(user.pwd);
-
-                var usr = new User()
-                {
-                    Username = user.username,
-                    Email = user.email,
-                    PasswordHash = passwordHash
-                };
-
-                ctx.Users.Add(usr);
-                if(await ctx.SaveChangesAsync() > 0)
-                {
-                    return usr.UserId;
-                }
+            _context.Users.Add(usr);
+            if(await _context.SaveChangesAsync() > 0)
+            {
+                return usr.UserId;
             }
 
             return Guid.NewGuid();
@@ -77,24 +71,20 @@ namespace Repository
 
         public async Task<bool> UnfollowUser(string username, string unfollows)
         {
-            //return true;
-            using (var ctx = new CustomDbContext())
-            {
-                var usr = ctx.Users.FirstOrDefault(u => u.Username == username);
-                var unflws = ctx.Users.FirstOrDefault(u => u.Username == unfollows);
+            var usr = _context.Users.FirstOrDefault(u => u.Username == username);
+            var unflws = _context.Users.FirstOrDefault(u => u.Username == unfollows);
 
-                var followers = await ctx.Followers
-                    .Include(f => f.Self)
-                    .Include(f => f.Following)
-                    .FirstOrDefaultAsync(f => f.Self.Username == username && f.Following.Username == unfollows);
+            var followers = await _context.Followers
+                .Include(f => f.Self)
+                .Include(f => f.Following)
+                .FirstOrDefaultAsync(f => f.Self.Username == username && f.Following.Username == unfollows);
 
-                if (followers == null)
-                    return false;
+            if (followers == null)
+                return false;
 
-                ctx.Followers.Remove(followers);
+            _context.Followers.Remove(followers);
 
-                return await ctx.SaveChangesAsync() != 0;
-            }
+            return await _context.SaveChangesAsync() != 0;
         }
     }
 }
