0c60fd406d60ec92cc6f351a98647abc4cdc1458
Philip <phko@itu.dk>
2020-02-20 15:49:31 +0100

    api db context setup

--- API/Minitwit.API/Startup.cs
+++ API/Minitwit.API/Startup.cs
@@ -1,17 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Infrastructure;
 using Infrastructure.Interfaces;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Minitwit.DataAccessLayer;
 using Repository;
 
 namespace Minitwit.API
@@ -32,6 +26,7 @@ namespace Minitwit.API
 
             services.AddScoped<IUserRepository, UserRepository>();
             services.AddScoped<ITimelineRepository, TimelineRepository>();
+            services.AddDbContext<CustomDbContext>(s => s.UseMySql(Configuration["ConnectionString"]));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
