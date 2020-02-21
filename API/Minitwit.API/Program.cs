f0f8db59d38b529dd0fe7394ff52363c0e90fec3
Marek Kisiel <mr.kislun@gmail.com>
2020-02-21 19:41:15 +0100

    temp

--- API/Minitwit.API/Program.cs
+++ API/Minitwit.API/Program.cs
@@ -18,6 +18,10 @@ namespace Minitwit.API
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureLogging(logging =>
+                {
+                    logging.AddConsole();
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
