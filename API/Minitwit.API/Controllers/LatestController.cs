0971a71a65656ed910f8f269dccdb9097540abb0
banksdev <alexbanks097@gmail.com>
2020-03-26 16:36:58 +0100

    add controller logging

--- API/Minitwit.API/Controllers/LatestController.cs
+++ API/Minitwit.API/Controllers/LatestController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Infrastructure.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using Minitwit.API.Util;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -14,16 +15,18 @@ namespace Minitwit.API.Controllers
     public class LatestController : Controller
     {
         private readonly ILastNumberRepository _lastNumberRepository;
+        private readonly ILogger<LatestController> _logger;
 
-        public LatestController(ILastNumberRepository lastNumberRepository)
+        public LatestController(ILastNumberRepository lastNumberRepository, ILogger<LatestController> logger)
         {
             _lastNumberRepository = lastNumberRepository;
+            _logger = logger;
         }
 
         [HttpGet("/latest")]
         public async Task<IActionResult> Get()
         {
-
+            _logger.LogInformation("Fetched latest endpoint");
             return new OkObjectResult(new { latest=  _lastNumberRepository.ReadLatest() });
         }
     }
