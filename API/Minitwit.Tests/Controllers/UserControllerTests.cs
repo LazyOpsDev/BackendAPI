e9c3bcd63dc203a1b2dd34fdc0de7e4ea1d4769f
Philip Korsholm <phko@itu.dk>
2020-03-29 17:04:32 +0200

    added authorization tests

---
+++ API/Minitwit.Tests/Controllers/UserControllerTests.cs
@@ -0,0 +1,53 @@
+﻿using Infrastructure.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Minitwit.API.Controllers;
+using Minitwit.API.Util;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Minitwit.Tests.Controllers
+{
+    public class UserControllerTests
+    {
+        [Fact]
+        public async Task FllwsRejectsUnauthorizedRequestsWith401Error()
+        {
+            var userRepositoryMock = new Mock<IUserRepository>();
+            var httpContext = new Microsoft.AspNetCore.Http.DefaultHttpContext(); // or mock a `HttpContext`
+            var controllerContext = new ControllerContext()
+            {
+                HttpContext = httpContext,
+            };
+
+            var controller = new UserController(userRepositoryMock.Object) { ControllerContext = controllerContext };
+
+            var res = await controller.fllws(new followModel(), "John Doe");
+
+            Assert.IsType<UnauthorizedResult>(res);
+        }
+
+        [Fact]
+        public async Task FllwsExecutesRequestMadeWithSimulatorHeader()
+        {
+            var userRepositoryMock = new Mock<IUserRepository>();
+            var httpContext = new Microsoft.AspNetCore.Http.DefaultHttpContext(); // or mock a `HttpContext`
+            httpContext.Request.Headers["Authorization"] = AuthorizationConstants.terribleHackAuth;
+            var controllerContext = new ControllerContext()
+            {
+                HttpContext = httpContext,
+            };
+
+            var controller = new UserController(userRepositoryMock.Object) { ControllerContext = controllerContext };
+
+            var res = await controller.fllws(new followModel(), "John Doe");
+
+            Assert.IsType<NoContentResult>(res);
+        }
+
+    }
+}
